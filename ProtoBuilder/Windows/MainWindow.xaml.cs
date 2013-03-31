df8992c886ca958defe7155bcfd8c3a2a0ec51e0
pdev <devolap@gmail.com>
2013-03-31 13:58:42 +0600

    Init

---
+++ ProtoBuilder/Windows/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtoBuilder.Windows {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
