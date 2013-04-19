6980ef0ab1e800f0463dd091346b3628665cab97
pdev <devolap@gmail.com>
2013-04-19 14:03:44 +0600

    Editor only

--- ProtoBuilder/Utils/Core.cs
+++ ProtoBuilder/Utils/Core.cs
@@ -1,11 +1,4 @@
 ﻿namespace ProtoBuilder.Utils {
     public class Core {
-        public static string GetMask(string value) {
-            var ret = "";
-            for (var i = 0; i < value.Length + 1; i++) {
-                ret = string.Format("{0}9", ret);
-            }
-            return ret;
-        }
     }
 }
